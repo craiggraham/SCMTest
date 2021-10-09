06eabfb2108f007e63670ec2de6aa703a495414f
Craig Graham <craig@craigshouse.net>
2021-10-09 11:59:36 +0100

    Added an interface to represent a person.

---
+++ BusinessRulesEngine/iPerson.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/* An interface to an object representing a person.
+ * Field names are self explanatory. We use the (almost) simplest case
+ * of single strings for the name and the address rather than subdividing
+ * into smaller components because there isn't a need to in this spec.
+ */
+
+namespace BusinessRulesEngine
+{
+    interface iPerson
+    {
+        string EmailAddress { get; set; }
+        string Name { get; set; }
+        string Address { get; set; }
+    }
+}
