7a8562a265f0b8c3368272a497a3b70710b02e7c
Craig Graham <craig@craigshouse.net>
2021-10-09 12:11:37 +0100

    Added enum and interface to represent a purchasable item, and moved things into a folder to keep things organised.

---
+++ BusinessRulesEngine/Interfaces/PurchasableTypeEnum.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/* An enumeration listing the different types of items that can be purchased.
+ * It's not an interface, but it's very strongly related to iPurchasable so it
+ * makes sense to be in here. Arguably it could be inside the iPurchasable file,
+ * but that adds to clutter and means that if the enum is changed to add a product
+ * type, it's not immediately clear that no other changes have been made to the
+ * interface.
+ */
+
+namespace BusinessRulesEngine.Interfaces
+{
+    enum PurchasableTypeEnum
+    {
+        Book,
+        PhysicalNonBook,
+        Membership,
+        MembershipUpgrade
+    }
+}
---
+++ BusinessRulesEngine/Interfaces/iPurchasable.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessRulesEngine.Interfaces
+{
+    /* Bare bones requirement for a Purchasable. 
+     * In reality there'd be at least a price and, so 
+     * we're not having to do string comparisons all the time,
+     * a numeric SKU.
+     *
+     */
+
+    interface iPurchasable
+    {
+        PurchasableTypeEnum ItemType { get; set; }
+        PurchasableTypeEnum ItemName { get; set; }
+    }
+}
