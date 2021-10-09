56a06301711b85447bb8ce412efa5931566848b5
Craig Graham <craig@craigshouse.net>
2021-10-09 12:27:01 +0100

    Added interfaces to represent orders, and to a component to send emails

---
+++ BusinessRulesEngine/Interfaces/iEmailManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessRulesEngine.Interfaces
+{
+    /* Interface to a component to manage the sending of emails.
+     * In a full system we'd also want an Email object and interface so
+     * we can specify the subject as well as the body but for this test
+     * that's overkill. We'd also have to think about whether it's appropriate
+     * to return something, but since most email failures happen asynchronously
+     * there's not a huge amount we can do with the information anyway.
+     */
+
+    interface iEmailManager
+    {
+        void SendEmail(iPerson person, String message);
+    }
+}
---
+++ BusinessRulesEngine/Interfaces/iOrder.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessRulesEngine.Interfaces
+{
+
+    /* Interface to represent an Order.
+     * We only need get here because it makes sense to
+     * specify the customer on object creation and we
+     * simplify item management by using a List with its
+     * own functions to add to and manipulate the contents.
+     */
+    interface iOrder
+    {
+        iPerson Customer { get; }
+
+        List<iPurchasable> Items { get; }
+    }
+}
