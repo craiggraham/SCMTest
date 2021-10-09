52ac6b31cf122c1b6fa380fce8fe827c7d40922a
Craig Graham <craig@craigshouse.net>
2021-10-09 13:29:11 +0100

    Started to implement the first business rule so there's something to actually test. Realised I need more test classes first!

---
+++ BusinessRulesEngine/Interfaces/Business logic/Strategy-PackingSlipForPhysicalProduct.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessRulesEngine.Interfaces.Business_logic
+{
+    /* A class to implement iPurchasableStrategy and do the first test,
+     * "If the payment is for a physical product, generate a packing slip for shipping."
+     */
+    class Strategy_PackingSlipForPhysicalProduct : iPurchasableStrategy
+    {
+        public void ApplyStrategy(iPurchasable item, iOrder order, iMembershipManager membershipManager, 
+            iCommissionManager commissionManager, iPackingSlipManager packingSlipManager)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
